ce3436d359daae85290fb66287b8745b97a3ef2c
Leonardo Montini <leonardo.montini@flowing.it>
2022-07-24 11:58:30 +0200

    proof of concept

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,5 +1,55 @@
+import { normalizeDuration } from "../";
+
 describe("normalize-duration", () => {
-  it("shall pass", () => {
-    expect(true).toBe(true);
+  it("converts milliseconds", () => {
+    // No action required
+    expect(normalizeDuration({ milliseconds: 10 })).toEqual({
+      milliseconds: 10,
+    });
+
+    // Jumps on the next unit
+    expect(normalizeDuration({ milliseconds: 1002 })).toEqual({
+      milliseconds: 2,
+      seconds: 1,
+    });
+
+    // Jumps on two units
+    expect(normalizeDuration({ milliseconds: 121005 })).toEqual({
+      milliseconds: 5,
+      seconds: 1,
+      minutes: 2,
+    });
+
+    // Jumps on three units + skip one
+    const millisInDay = 24 * 60 * 60 * 1000;
+    expect(
+      normalizeDuration({ milliseconds: 121005 + millisInDay * 3 })
+    ).toEqual({
+      milliseconds: 5,
+      seconds: 1,
+      minutes: 2,
+      hours: 0,
+      days: 3,
+    });
+  });
+
+  it("converts seconds skipping milliseconds", () => {
+    // No action required
+    expect(normalizeDuration({ seconds: 10 })).toEqual({
+      seconds: 10,
+    });
+
+    // Jumps on the next unit
+    expect(normalizeDuration({ seconds: 75 })).toEqual({
+      seconds: 15,
+      minutes: 1,
+    });
+  });
+
+  it("correctly handles the last unit - years", () => {
+    expect(normalizeDuration({ months: 25 })).toEqual({
+      months: 1,
+      years: 2,
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,65 @@
-export const normalizeDuration = () => {
-  return true;
+export type Duration = {
+  years?: number;
+  months?: number;
+  days?: number;
+  hours?: number;
+  minutes?: number;
+  seconds?: number;
+  milliseconds?: number;
+};
+
+type Concrete<Type> = {
+  [Property in keyof Type]-?: Type[Property];
+};
+
+const factors: Concrete<Omit<Duration, "years">> = {
+  months: 12,
+  days: 30,
+  hours: 24,
+  minutes: 60,
+  seconds: 60,
+  milliseconds: 1000,
+};
+
+const order: Array<keyof typeof factors> = [
+  "months",
+  "days",
+  "hours",
+  "minutes",
+  "seconds",
+  "milliseconds",
+];
+
+export const normalizeDuration = (duration: Duration): Duration => {
+  const { normalizedDuration, rest } = order.reduceRight(
+    ({ normalizedDuration, rest }, key) => {
+      const currentUnit = (duration[key] || 0) + rest;
+      const currentFactor = factors[key];
+
+      if (currentUnit > 0) {
+        return {
+          normalizedDuration: {
+            ...normalizedDuration,
+            [key]: currentUnit % currentFactor,
+          },
+          rest: Math.floor(currentUnit / currentFactor),
+        };
+      } else {
+        return {
+          normalizedDuration,
+          rest: Math.floor(currentUnit / currentFactor),
+        };
+      }
+    },
+    { normalizedDuration: {}, rest: 0 }
+  );
+
+  if (rest > 0) {
+    return {
+      ...normalizedDuration,
+      years: rest,
+    };
+  }
+
+  return normalizedDuration;
 };
